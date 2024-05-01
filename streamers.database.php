89279e1ff0e8c82cc195a1997735257cb7087f6e
MatheoV1218 <129867668+MatheoV1218@users.noreply.github.com>
2024-05-01 17:03:34 -0400

    Update streamers.database.php

--- streamers.database.php
+++ streamers.database.php
@@ -1,11 +1,56 @@
 <!DOCTYPE html>
 <html>
-<style>
-    body
-    {
+  <style>
+    body{
       background-color: rgb(157, 212, 224);
+
     }
-</style>
-<head></head>
-<body></body>
-<html>
+    .table{
+  border-collapse: collapse;
+  width: 100%;
+  color: #d96459;
+  font-family: monospace;
+  font-size: 25px;
+  text-align: left;
+  overflow: hidden;
+  margin-bottom: 20px;
+}
+.p{
+  text-align: center;
+  font-size: 75px;
+  font-family: times new roman;
+}
+    </style>
+    <p class="p">Developer database</p>
+  <body>
+    <table class="table">
+      <tr>
+        <th class="th">Name</th>
+        <th class="th">Streaming service</th>
+        <th class="th">Content Genre</th>
+      </tr>
+      <?php
+        $conn = mysqli_connect('127.0.0.1:3377', 'root', '', 'myfirstdatabase');
+        if ($conn-> connect_error)
+        {
+          die("Coneection failed:". $conn-> connect_error);
+        }
+        $sql = "SELECT name, streaming_service, content_genre from entertainer";
+        $result = $conn-> query($sql);
+        if ($result-> num_rows > 0)
+        {
+          while ($row = $result->fetch_assoc())
+          {
+            echo "<tr><td>". $row["name"] ."</td><td>". $row["streaming_service"] . "</td><td>". $row["content_genre"] ."</td></tr>";
+          }
+          echo "</table>";
+        }
+        else
+        {
+          echo "0 result";
+        }
+        $conn-> close();
+      ?>
+    </table>
+  </body>
+</html>
