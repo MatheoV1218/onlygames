fd9a6fff9c83a0b6520a815bb126c30e8bffe375
MatheoV1218 <129867668+MatheoV1218@users.noreply.github.com>
2024-05-01 17:59:09 -0400

    Update developers.database.php

--- developers.database.php
+++ developers.database.php
@@ -1,21 +1,43 @@
 <!DOCTYPE html>
 <html>
-  <head>Developer Database</head>
   <style>
     body{
-      background-color: lightpink;
+      background-color: rgb(157, 212, 224);
 
     }
+    .table{
+  border-collapse: collapse;
+  width: 100%;
+  color: #d96459;
+  font-family: monospace;
+  font-size: 25px;
+  text-align: left;
+  overflow: hidden;
+  margin-bottom: 20px;
+}
+.p{
+  text-align: center;
+  font-size: 75px;
+  font-family: times new roman;
+}
     </style>
+    <div>
+      <a href="classproject.php#home">
+        <button class="click">
+        <img src="Class project onlygames/left arrow.png" style="width: 50px; height: 50px;">
+        </button>
+      </a>
+    </div>
+    <p class="p">Developer database</p>
   <body>
-    <table>
+    <table class="table">
       <tr>
-        <th>Name</th>
-        <th>Location</th>
-        <th>Website</th>
+        <th class="th">Name</th>
+        <th class="th">Location</th>
+        <th class="th">Website</th>
       </tr>
       <?php
-        $conn = mysqli_connect('localhost', 'root', '', 'myfirstdatabase');
+        $conn = mysqli_connect('127.0.0.1:3377', 'root', '', 'myfirstdatabase');
         if ($conn-> connect_error)
         {
           die("Coneection failed:". $conn-> connect_error);
