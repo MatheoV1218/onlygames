1ae091880764c7c76d9906c6e3d3faf6f504d427
MatheoV1218 <129867668+MatheoV1218@users.noreply.github.com>
2024-05-03 11:17:24 -0400

    Update streamers.php

--- streamers.php
+++ streamers.php
@@ -18,12 +18,27 @@
       font-size: 20px;
       font-family: Arial, Roboto;
       margin: 0;
-      cursor: pointer;
-      
+      cursor: pointer; 
     }
+  
+    .game-title{
+  font-family: Times New Roman;
+  font-size: 25px;
+  margin-top: 0;
+  text-align: center;
+  color: black;
+  font-weight: 500;
+}
   </style>
   <head></head>
   <body>
+     <div>
+      <a href="classproject.php#home">
+        <button class="click">
+        <img src="Class project onlygames/left arrow.png" style="width: 50px; height: 50px;">
+        </button>
+      </a>
+    </div>
   <div>
       <p class="streamers">
         Streamers
@@ -35,5 +50,146 @@
       </div>
     </div>
    
+
+    <div style="
+    margin-top: 30px;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    column-gap: 20px;
+    row-gap: 40px;">
+    <div>
+      <img src="Class project onlygames/esports/clix.jpg" style="width: 350px; height: 300px;">
+        <p class="game-title">Clix</p>
+    </div>
+    <div>
+      <img src="Class project onlygames/esports/CourageJD.jpg" style="width: 350px; height: 300px;">
+       <p class="game-title">CourageJD</p>
+    </div>
+    <div>
+      <img src="Class project onlygames/esports/dantes.jpg" style="width: 350px; height: 300px;">
+       <p class="game-title">Dantes</p>
+    </div>
+   
+   
+    </div>
+
+<div style="
+    margin-top: 30px;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    column-gap: 20px;
+    row-gap: 40px;">
+         <div>
+          <img src="Class project onlygames/esports/drdisrepect.jpg" style="width: 350px; height: 300px;">
+           <p class="game-title">Dr.Disrespect</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/esports/jshaltt.jpg" style="width: 350px; height: 300px;">
+            <p class="game-title">Jschlatt</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/jynxzi.webp" style="width: 350px; height: 300px;">\
+           <p class="game-title">Jynxzi</p>
+        </div>
+    </div>
+
+    <div style="
+    margin-top: 30px;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    column-gap: 20px;
+    row-gap: 40px;">
+         <div>
+          <img src="Class project onlygames/critical.jpg" style="width: 350px; height: 300px;">
+           <p class="game-title">Moistcr1tikal</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/mongral.jpg" style="width: 350px; height: 300px;">
+            <p class="game-title">Mongraal</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/myth.jpg" style="width: 350px; height: 300px;">\
+           <p class="game-title">Myth</p>
+        </div>
+    </div>
+
+    <div style="
+    margin-top: 30px;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    column-gap: 20px;
+    row-gap: 40px;">
+         <div>
+          <img src="Class project onlygames/nickmercs.jpg" style="width: 350px; height: 300px;">
+           <p class="game-title">Nickmercs</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/ninja.jpg" style="width: 350px; height: 300px;">
+            <p class="game-title">Ninja</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/OHNEPIXEL.jpg" style="width: 350px; height: 300px;">\
+           <p class="game-title">OHNEPIXEL</p>
+        </div>
+    </div>
+
+    <div style="
+    margin-top: 30px;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    column-gap: 20px;
+    row-gap: 40px;">
+         <div>
+          <img src="Class project onlygames/philza.png" style="width: 350px; height: 300px;">
+           <p class="game-title">Philza</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/shroud.jpg" style="width: 350px; height: 300px;">
+            <p class="game-title">Shroud</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/sketch.jpg" style="width: 350px; height: 300px;">\
+           <p class="game-title">Sketch</p>
+        </div>
+    </div>
+
+    <div style="
+    margin-top: 30px;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    column-gap: 20px;
+    row-gap: 40px;">
+         <div>
+          <img src="Class project onlygames/summit1g.jpg" style="width: 350px; height: 300px;">
+           <p class="game-title">Summit1g</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/tfue.jpg" style="width: 350px; height: 300px;">
+            <p class="game-title">Tfue</p>
+        </div>
+        <div>
+          <img src="Class project onlygames/timthetatman.jpg" style="width: 350px; height: 300px;">\
+           <p class="game-title">TimTheTatman</p>
+        </div>
+    </div>
+
   </body>
 </html>
